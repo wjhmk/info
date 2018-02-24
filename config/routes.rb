005d960227b35cb5d7afe1fb35685aa2d2ec3453
ren <wjhmkrenren@icloud.com>
2018-02-24 10:19:35 +0000

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "posts#index"
   
+  resources :users, :only => [:index, :show]
+  
   resources :posts do
     resources :comments
   end
   
 end
+
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180213161032) do
+ActiveRecord::Schema.define(version: 20180224095153) do
 
   create_table "comments", force: :cascade do |t|
     t.text "name"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 20180213161032) do
     t.datetime "locked_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
